e42f8d8ce0dcf0ffa43c6d84fc3bae74240e6e77
Jeremy Villa <villajeremy1991@gmail.com>
2023-10-27 22:53:11 -0400

    Moved account create and update to service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,21 +1,26 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService, AccountService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  //@Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService,
+    private accountService: AccountService) {}
+
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
+    this.accountService.addAccount(accountName, accountStatus);
+
     // console.log('A server status changed, new status: ' + accountStatus);
     //Replaced the above code with the following:
     this.loggingService.logAccountStatusChange(accountStatus);
